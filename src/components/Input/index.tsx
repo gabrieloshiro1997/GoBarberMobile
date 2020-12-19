cfb61f61c3ba4b4a6e69830c30e3ea68b20744b6
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2020-12-19 15:02:43 -0300

    fix: adjustment on input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -8,15 +8,18 @@ interface InputProps extends TextInputProps {
   icon: string;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => (
-  <Container>
-    <Icon name={icon} size={20} color="#666360" />
-    <TextInput
-      keyboardAppearance="dark"
-      placeholderTexColor="#666360"
-      {...rest}
-    />
-  </Container>
-);
+const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
+  return (
+    <Container>
+      <Icon name={icon} size={20} color="#666360" />
+
+      <TextInput
+        keyboardAppearance="dark"
+        placeholderTextColor="#666360"
+        {...rest}
+      />
+    </Container>
+  );
+};
 
 export default Input;
